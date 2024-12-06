8a0be64df3948f3a5e7ced80b298dc4d32861a5c
lavanya <iynxr.dev@gmail.com>
2024-12-06 20:51:26 +1100

    nav-udpates

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,9 @@
-export default function TechStack() {
-    const tools = ["React", "Next.js", "TypeScript", "Tailwind CSS", "Three.js"];
+export default function Projects() {
+    const tools = ["Management Tools", "GEO Location Project", "Chat Rep", "Game hub", "The UI Framework"];
   
     return (
       <div className="p-6 sm:p-12">
-        <h2 className="text-2xl sm:text-3xl font-bold mb-8">TECH STACK</h2>
+        <h2 className="text-2xl sm:text-3xl font-bold mb-8">PROJECTS</h2>
         <div className="section-divider"></div>
         <ul className="grid grid-cols-2 md:grid-cols-3 gap-6">
           {tools.map((tool) => (
@@ -15,7 +15,6 @@ export default function TechStack() {
             </li>
           ))}
         </ul>
-        
       </div>
     );
   }
